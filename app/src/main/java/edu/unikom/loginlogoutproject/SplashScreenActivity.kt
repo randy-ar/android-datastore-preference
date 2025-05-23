7fbfe3cd269b30200d22169923c745d2dd456eef
randy-ar <randyabdulrahman87@gmail.com>
2025-05-23 14:25:20 +0700

    first commit

---
+++ app/src/main/java/edu/unikom/loginlogoutproject/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package edu.unikom.loginlogoutproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import edu.unikom.loginlogoutproject.databinding.ActivitySplashScreenBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Fungsi Slide Animation
+        val slideIn = AnimationUtils.loadAnimation(this, R.anim.slide_in)
+        binding.textLoading.startAnimation(slideIn)
+        binding.textNamaAplikasi.startAnimation(slideIn)
+
+        lifecycleScope
+            .launch {
+                delay(5000)
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                finish()
+            }
+    }
+}
